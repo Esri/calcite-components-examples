5870330d64a158c84e65daf0ce4f73ccd1b4a23b
Paul C Pederson <paul.c.pederson@gmail.com>
2020-07-16 16:30:59 -0700

    add better example for using events in react #1

--- react/src/App.js
+++ react/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '@esri/calcite-components/dist/calcite/calcite.css';
 import './App.css';
 
 function App() {
+  const sliderEl = useRef(null);
+  const [sliderValue, setSliderValue] = useState(50);
+
+  function onUpdate(event){
+    setSliderValue(event.target.value);
+  }
+
+  // need to access the dom node to set custom event listeners or props that aren't strings / numbers
+  // https://stenciljs.com/docs/react#properties-and-events
+  useEffect(_ => {
+    sliderEl.current.addEventListener('calciteSliderUpdate', onUpdate);
+  }, [sliderEl]);
+
   return (
     <div className="App">
       <h1>Hello, React</h1>
       <calcite-button>Test</calcite-button>
       <calcite-icon icon="banana" />
+      <calcite-slider
+        ref={sliderEl}
+        min="1"
+        max="100"
+        value={sliderValue}
+        step="1"/>
+      <p>The slider currently has a value of {sliderValue}</p>
     </div>
   );
 }
