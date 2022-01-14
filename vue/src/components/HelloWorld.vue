89412f15c8506b0df3ecb2b62c6c067aeab522dd
Ben Elan <belan@esri.com>
2022-01-14 14:48:39 -0800

    chore(vue): update example

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }} <calcite-icon icon="banana"></calcite-icon></h1>
+    <calcite-date-picker></calcite-date-picker>
     <calcite-button>Button</calcite-button>
-    <calcite-icon icon="banana"></calcite-icon>
   </div>
 </template>
 
 <script>
+import "@esri/calcite-components/dist/components/calcite-button";
+import "@esri/calcite-components/dist/components/calcite-icon";
+import "@esri/calcite-components/dist/components/calcite-date-picker";
+
 export default {
   name: "HelloWorld",
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 };
 </script>
+
+<style src="@esri/calcite-components/dist/calcite/calcite.css"></style>
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,15 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import {
-  applyPolyfills,
-  defineCustomElements
-} from "@esri/calcite-components/dist/loader";
+import { setAssetPath } from '@esri/calcite-components/dist/components';
 
-// Apply polyfills and then define the custom elements
-// polyfills are not needed if you don't support IE11 or Edge
-applyPolyfills().then(() => {
-  defineCustomElements(window);
-});
+setAssetPath(location.href);
 
 // tell Vue.js to ignore Calcite Components
 Vue.config.ignoredElements = [/calcite-\w*/];
