f82ba30febb357410d8dfc5bee971683d6765898
Kitty Hurley <khurley@esri.com>
2022-04-08 10:33:20 -0500

    feat(ng): new angular 13 example
    previous version was angular 9

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,15 +1,19 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
--- angular/src/main.ts
+++ angular/src/main.ts
@@ -4,14 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// Set Calcite asset path for icons and locales
+// Calcite Components assets
+// import { setAssetPath } from "@esri/calcite-components/dist/components";
+// setAssetPath(location.href);
+
 import { setAssetPath } from "@esri/calcite-components/dist/components";
-setAssetPath(location.href)
+setAssetPath(location.href);
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
